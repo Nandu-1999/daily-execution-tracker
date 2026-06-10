93c3b2c32298528472211cc1a91eaa9b7daade75
Nandu <knandu378@gmail.com>
2026-06-10 12:24:49 +0530

    Add default database URL for CI

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,10 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./ci.db"
+)
 
 SECRET_KEY = os.getenv(
     "SECRET_KEY",
